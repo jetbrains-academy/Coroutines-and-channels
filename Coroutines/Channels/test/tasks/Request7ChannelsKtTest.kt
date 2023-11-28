8b54f3eaeca03f20e49dfc965a18c88707520a91
Konstantin Chaika <konstantin.chaika@jetbrains.com>
2023-11-28 16:54:23 +0400

    Add check that solution is not empty or do nothing

--- Coroutines/Channels/test/tasks/Request7ChannelsKtTest.kt
+++ Coroutines/Channels/test/tasks/Request7ChannelsKtTest.kt
@@ -3,25 +3,31 @@ package tasks
 import contributors.MockGithubService
 import contributors.concurrentProgressResults
 import contributors.testRequestData
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.currentTime
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Test
 
 class Request7ChannelsKtTest {
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun testChannels() = runBlocking {
-        val startTime = System.currentTimeMillis()
+    fun testChannels() = runTest {
+        val startTime = currentTime
         var index = 0
+        var resultUpdated = false
         loadContributorsChannels(MockGithubService, testRequestData) {
                 users, _ ->
             val expected = concurrentProgressResults[index++]
-            val time = System.currentTimeMillis() - startTime
-            /*
-            // TODO: uncomment this assertion
-            Assert.assertEquals("Expected intermediate result after virtual ${expected.timeFromStart} ms:",
-                expected.timeFromStart, time)
-            */
+            val time = currentTime - startTime
+            resultUpdated = true
+
             Assert.assertEquals("Wrong intermediate result after $time:", expected.users, users)
         }
+
+        delay(1000) // Enough, since requests through the MockGithubService uses virtual time in delay
+        Assert.assertTrue("loadContributorsChannels function should update results", resultUpdated)
     }
 }
--- Coroutines/Showing progress/test/tasks/Request6ProgressKtTest.kt
+++ Coroutines/Showing progress/test/tasks/Request6ProgressKtTest.kt
@@ -3,26 +3,32 @@ package tasks
 import contributors.MockGithubService
 import contributors.progressResults
 import contributors.testRequestData
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.currentTime
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Test
 import tasks.loadContributorsProgress
 
 class Request6ProgressKtTest {
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun testProgress() = runBlocking {
-        val startTime = System.currentTimeMillis()
+    fun testProgress() = runTest {
+        val startTime = currentTime
         var index = 0
+        var progressUpdated = false
         loadContributorsProgress(MockGithubService, testRequestData) {
             users, _ ->
             val expected = progressResults[index++]
-            val time = System.currentTimeMillis() - startTime
-            /*
-            // TODO: uncomment this assertion
-            Assert.assertEquals("Expected intermediate result after virtual ${expected.timeFromStart} ms:",
-                expected.timeFromStart, time)
-            */
+            val time = currentTime - startTime
+            progressUpdated = true
+
             Assert.assertEquals("Wrong intermediate result after $time:", expected.users, users)
         }
+
+        delay(1000) // Enough, since requests through the MockGithubService uses virtual time in delay
+        Assert.assertTrue("loadContributorsProgress function should update results", progressUpdated)
     }
 }
