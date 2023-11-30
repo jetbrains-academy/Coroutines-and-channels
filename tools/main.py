71c17603602aa22fef5e8c713be0bd988831db00
Konstantin Chaika <konstantin.chaika@jetbrains.com>
2023-11-30 11:02:49 +0400

    More verbose output

--- tools/main.py
+++ tools/main.py
@@ -51,33 +51,57 @@ if __name__ == '__main__':
     task_description_name = 'task.md'
     course_link_prefix = 'course://'
 
-    print(f"Running for directory: {os.path.abspath(course_directory)}")
+    print("\n===== Common info =====\n")
+    print(f"Running for directory {course_directory} a.k.a.  {os.path.abspath(course_directory)}")
 
     task_files = find_files(course_directory, task_description_name)
 
-    print(f"The following task files were found: {task_files}")
+    print(f"\n===== The following links were found and processing =====\n")
+    errors_log = ""
 
     for file in task_files:
+        print(f"FILE: {file}")
         links, images = extract_links_from_markdown(read_file(file))
         task_folder = file[:file.rfind("/")]
 
         # checking links to local files and web resources
         for link in links:
+            print(f"\t LINK: {link}")
             if link.startswith(course_link_prefix):
                 internal_resource_link = link[len(course_link_prefix):]
                 internal_resource_path = f"{course_directory}/{internal_resource_link}"
                 if not (os.path.isfile(internal_resource_path) or os.path.isdir(internal_resource_path)):
                     print(f"Error in file: {file}")
                     print(f"\tNO such file: {internal_resource_path}")
+                    errors_log+=f"Error in file: {file}\n\tNO such file: {internal_resource_path}"
             else:  # try to interpret link as an url
                 response = requests.head(link)
                 if response.status_code != 200:
                     print(f"Error in file: {file}")
                     print(f"\tNOT valid url (returns {response.status_code}): {link}")
+                    errors_log+=f"Error in file: {file}\n\tNOT valid url (returns {response.status_code}): {link}"
 
         # checking images
         for image in images:
+            print(f"\t IMAGE: {image}")
             image_path = f"{task_folder}/{image}"
             if not os.path.isfile(image_path):
                 print(f"Error in file: {file}")
                 print(f"\tNO such file: {image_path}")
+                errors_log+=f"Error in file: {file}\n\tNO such file: {image_path}"
+
+    print("\n===== ERRORS LOG =====\n")
+    if len(errors_log) > 0:
+        print(errors_log)
+        exit(1)
+    else:
+        print("No errors found")
+        exit(0)
+
+# TODO:
+# BUG: image links can be URLs
+# Support another types of links:
+    # file://
+    # psi_element://
+    # tool_window://
+    # settings://
